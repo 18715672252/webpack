8351a86c1d3304d321322f72f383271d2e8eca48
孙成龙 <51355379+18715672252@users.noreply.github.com>
2020-04-23 10:10:56 +0800

    修改提取css的插件名字

--- plugin.js
+++ plugin.js
@@ -2,7 +2,7 @@
 常用的plugin
 1.commons-chunk-plugin 将chunks相同的模块代码提取成公共的JS
 2.clean-webpack-plugin 清理构建目录
-3.extract-text-webpack-plugin 将css从打包后js文件提取成一个css文件
+3.mini-css-extract-webpack-plugin 将css从打包后js文件提取成一个css文件
 4.copy-webpack-plugin 将文件或者文件夹拷贝到构建的输出目录
 5.html-webpack-plug 创建html文件去承载打包后的js和css文件
 6.uglifyjs-webpack-plugin 压缩js
